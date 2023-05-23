6a57f3d4a2d301450ed8669f79c96cd7554908ad
ilyakrivonozhko <ilya.krivonozhko@gmail.com>
2023-05-23 15:09:14 +0800

    Added the ability to remember the user in cookies.

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -4,7 +4,23 @@ module Authentication
         private
 
         def current_user
-            @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
+            user = session[:user_id].present? ? user_from_session : user_from_token
+
+            @current_user ||= user
+        end
+
+        def user_from_session
+            User.find_by(id: session[:user_id])
+        end
+      
+        def user_from_token
+            user = User.find_by(id: cookies.encrypted[:user_id])
+            token = cookies.encrypted[:remember_token]
+        
+            return unless user&.remember_token_authenticated?(token)
+        
+            sign_in user
+            user
         end
     
         def user_signed_in?
@@ -16,6 +32,7 @@ module Authentication
         end
 
         def sign_out
+            forget current_user
             session.delete :user_id
         end
 
@@ -29,6 +46,18 @@ module Authentication
             return if user_signed_in?
             flash[:warning] = "You are not signed in!"
             redirect_to root_path
+        end
+
+        def remember(user)
+            user.remember_me
+            cookies.encrypted.permanent[:remember_token] = user.remember_token
+            cookies.encrypted.permanent[:user_id] = user.remember_token
+        end
+
+        def forget(user)
+            user.forget_me
+            cookies.delete :user_id
+            cookies.delete :remember_token
         end
         
         helper_method :current_user, :user_signed_in?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_020110) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_063552) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_020110) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_toker_digest"
+    t.string "remember_token_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
   end
